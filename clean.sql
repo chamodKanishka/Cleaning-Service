fbd6d5b8403e8a93fd2fbe7faf386dd72eb9bad6
ChamodKanishka <chamodkanishka77@gmail.com>
2020-04-15 12:36:05 +0530

    sql file update

--- clean.sql
+++ clean.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 09, 2020 at 07:54 AM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.0.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 15, 2020 at 07:05 AM
+-- Server version: 8.0.18
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,13 +28,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `contact`
 --
 
-CREATE TABLE `contact` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `sub` varchar(50) NOT NULL,
-  `msg` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `msg` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `contact`
@@ -50,12 +52,14 @@ INSERT INTO `contact` (`id`, `name`, `email`, `sub`, `msg`) VALUES
 -- Table structure for table `customer`
 --
 
-CREATE TABLE `customer` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE IF NOT EXISTS `customer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `image` varchar(250) NOT NULL,
-  `msg` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `msg` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
@@ -71,11 +75,13 @@ INSERT INTO `customer` (`id`, `name`, `image`, `msg`) VALUES
 -- Table structure for table `hospital`
 --
 
-CREATE TABLE `hospital` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `hospital`;
+CREATE TABLE IF NOT EXISTS `hospital` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `hospital`
@@ -93,15 +99,17 @@ INSERT INTO `hospital` (`id`, `name`, `image`) VALUES
 -- Table structure for table `offer`
 --
 
-CREATE TABLE `offer` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `offer`;
+CREATE TABLE IF NOT EXISTS `offer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `surname` varchar(50) NOT NULL,
   `phone` varchar(30) NOT NULL,
   `email` varchar(50) NOT NULL,
   `question` varchar(250) NOT NULL,
-  `c_type` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `c_type` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `offer`
@@ -109,7 +117,8 @@ CREATE TABLE `offer` (
 
 INSERT INTO `offer` (`id`, `name`, `surname`, `phone`, `email`, `question`, `c_type`) VALUES
 (1, 'Wijesooriya', 'lahiru', '098765432345', 'lahiru@gmail.com', 'Hi,\r\nGive more details about hospital cleaning.\r\nthank you.', ''),
-(2, 'Wijesooriya', 'lahiru', '098765432345', 'lahiru@gmail.com', 'Hi,\r\nGive more details about hospital cleaning.\r\nthank you.', '');
+(2, 'Wijesooriya', 'lahiru', '098765432345', 'lahiru@gmail.com', 'Hi,\r\nGive more details about hospital cleaning.\r\nthank you.', ''),
+(3, 'chamod', 'kanishka', '0756618720', 'chamodkanishka77@gmail.com', 'nothing', '2');
 
 -- --------------------------------------------------------
 
@@ -117,11 +126,13 @@ INSERT INTO `offer` (`id`, `name`, `surname`, `phone`, `email`, `question`, `c_t
 -- Table structure for table `office`
 --
 
-CREATE TABLE `office` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `office`;
+CREATE TABLE IF NOT EXISTS `office` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `office`
@@ -138,11 +149,13 @@ INSERT INTO `office` (`id`, `name`, `image`) VALUES
 -- Table structure for table `places`
 --
 
-CREATE TABLE `places` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `places`;
+CREATE TABLE IF NOT EXISTS `places` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `places`
@@ -150,8 +163,8 @@ CREATE TABLE `places` (
 
 INSERT INTO `places` (`id`, `name`, `image`) VALUES
 (1, 'Hospitals &retirement homes', 'assets/images/place/hos.png'),
-(2, 'Supermarkets & retail', 'assets/images/place/sup.jpg'),
-(3, 'Offices', 'assets/images/place/off.jpg'),
+(2, 'Supermarkets & retail', 'assets/images/place/sup.png'),
+(3, 'Offices', 'assets/images/place/off.png'),
 (4, 'Public places', 'assets/images/place/pla.png');
 
 -- --------------------------------------------------------
@@ -160,11 +173,13 @@ INSERT INTO `places` (`id`, `name`, `image`) VALUES
 -- Table structure for table `public`
 --
 
-CREATE TABLE `public` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `public`;
+CREATE TABLE IF NOT EXISTS `public` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `public`
@@ -181,21 +196,23 @@ INSERT INTO `public` (`id`, `name`, `image`) VALUES
 -- Table structure for table `servise`
 --
 
-CREATE TABLE `servise` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `servise`;
+CREATE TABLE IF NOT EXISTS `servise` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `image` varchar(250) NOT NULL,
   `ser_name` varchar(200) NOT NULL,
   `description` varchar(250) NOT NULL,
-  `website` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `website` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `servise`
 --
 
 INSERT INTO `servise` (`id`, `image`, `ser_name`, `description`, `website`) VALUES
-(1, 'assets/images/service/img_4.jpg', '', 'Regular cleanng, also known as a cleaning team cleaning your offices, floors, windows and much more. ', 'servise-details-corona.php'),
-(2, 'assets/images/service/img_3.jpg', '', 'Special cleaning, such as carpets, boning, roofs, solar panels and the more complex situations. ', 'services-details.php'),
+(1, 'assets/images/service/img_4.png', '', 'Regular cleanng, also known as a cleaning team cleaning your offices, floors, windows and much more. ', 'servise-details-corona.php'),
+(2, 'assets/images/service/img_3.png', '', 'Special cleaning, such as carpets, boning, roofs, solar panels and the more complex situations. ', 'services-details.php'),
 (3, 'assets/images/service/img_2.png', '', 'Detail cleaning and disinfection for hospitals, retirement homes, retail and supermarkets, offices and public places. This deep dive cleaning requires more care and knowledge.  ', 'services-details.php'),
 (4, 'assets/images/service/img_1.png', '', 'Pest control isn’t just catching some pigeons or rats, but making sure they and many other little dirty animals don’t come back. ', 'services-details.php');
 
@@ -205,8 +222,9 @@ INSERT INTO `servise` (`id`, `image`, `ser_name`, `description`, `website`) VALU
 -- Table structure for table `staff`
 --
 
-CREATE TABLE `staff` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `staff`;
+CREATE TABLE IF NOT EXISTS `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `position` varchar(100) NOT NULL,
   `description` varchar(250) NOT NULL,
@@ -214,8 +232,9 @@ CREATE TABLE `staff` (
   `tw` varchar(100) NOT NULL,
   `ln` varchar(100) NOT NULL,
   `yt` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff`
@@ -233,11 +252,13 @@ INSERT INTO `staff` (`id`, `name`, `position`, `description`, `fb`, `tw`, `ln`, 
 -- Table structure for table `supermarket`
 --
 
-CREATE TABLE `supermarket` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `supermarket`;
+CREATE TABLE IF NOT EXISTS `supermarket` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `image` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image` varchar(250) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `supermarket`
@@ -248,134 +269,6 @@ INSERT INTO `supermarket` (`id`, `name`, `image`) VALUES
 (2, 'Store surface  ', 'assets\\images\\Icons\\Market/stock.png'),
 (3, 'Stock area  ', 'assets\\images\\Icons\\Market/shops.png'),
 (4, 'Shopping cards  ', 'assets\\images\\Icons\\Market/debit-card.png');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `hospital`
---
-ALTER TABLE `hospital`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `offer`
---
-ALTER TABLE `offer`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `office`
---
-ALTER TABLE `office`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `places`
---
-ALTER TABLE `places`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `public`
---
-ALTER TABLE `public`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `servise`
---
-ALTER TABLE `servise`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `staff`
---
-ALTER TABLE `staff`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `supermarket`
---
-ALTER TABLE `supermarket`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `contact`
---
-ALTER TABLE `contact`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `hospital`
---
-ALTER TABLE `hospital`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `offer`
---
-ALTER TABLE `offer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `office`
---
-ALTER TABLE `office`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `places`
---
-ALTER TABLE `places`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `public`
---
-ALTER TABLE `public`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `servise`
---
-ALTER TABLE `servise`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `staff`
---
-ALTER TABLE `staff`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `supermarket`
---
-ALTER TABLE `supermarket`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
